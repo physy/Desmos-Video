f25042097b6ff747b8c365b8d0a60b66d68834c8
physy <138382059+physy@users.noreply.github.com>
2025-09-07 03:16:14 +0900

    不要コードの削除

--- src/App.tsx
+++ src/App.tsx
@@ -705,7 +705,6 @@ function App() {
           label: "バージョン情報",
           onClick: () => {
             alert("Desmos Video Creator v1.0");
-            alert("Desmos Video Creator v1.0");
             closeAllMenus();
           },
         },
